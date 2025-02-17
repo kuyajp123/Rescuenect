89aba2902c5537b4fe37b730e159c01bc2536055
kuyajp123 <kuyajp123@gmail.com>
2025-02-17 23:09:55 +0800

    New

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,20 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
 import App from './App.tsx'
-import Try from './Try.tsx'
-
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' element={<App />}></Route>
-        <Route path='/Try' element={<Try />}></Route>
-      </Routes>
-      
-    </Router>
+    <App />
   </StrictMode>,
 )
