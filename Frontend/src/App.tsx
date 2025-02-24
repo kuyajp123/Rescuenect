7819231dff866dec06cbd8284f3bf73326fa51f0
kuyajp123 <kuyajp123@gmail.com>
2025-02-24 23:31:50 +0800

    temporary layout

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,32 @@
 import Test from "./pages/Test";
 import { ThemeSwitcher } from "./pages/ThemeSwitcher";
 
+// const child = {
+//   height: "100%",
+//   width: "100%",
+//   display: "flex",
+//   justifyContent: "center",
+//   alignItems: "center",
+//   border: "1px solid red",
+// };
+
+// const parent = {
+//   height: "100%",
+//   width: "100%",
+//   display: "flex",
+//   justifyContent: "center",
+//   alignItems: "center",
+//   border: "1px solid red",
+// };
+
 function App() {
   return (
-  <div className="font-sans">
-    <Test />
+  <div className="font-sans container">
     <ThemeSwitcher />
+    <div className="container">
+      <Test />
+      asdasd
+    </div>
   </div>
   );
 }
--- Frontend/src/pages/ThemeSwitcher.tsx
+++ Frontend/src/pages/ThemeSwitcher.tsx
@@ -5,10 +5,9 @@ export const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme()
 
   return (
-    <div>
+    <div className="flex justify-end items-center gap-3">
       The current theme is: {theme}
       <PrimaryButton className="primary" onClick={() => setTheme('light')}>Light Mode</PrimaryButton>
-      &nbsp;&nbsp;&nbsp;&nbsp;
       <PrimaryButton className="primary text-white" onClick={() => setTheme('dark')}>Dark Mode</PrimaryButton>
     </div>
   )
