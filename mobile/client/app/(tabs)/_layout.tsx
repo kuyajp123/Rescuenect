f69a9240a3c1e6e2bc5e71a7b76faa49e5052447
kuyajp123 <kuyajp123@gmail.com>
2025-05-16 12:56:17 +0800

    setup client side for mobile app

---
+++ mobile/client/app/(tabs)/_layout.tsx
@@ -0,0 +1,30 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function TabLayout() {``
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#0a7ea4',
+        headerShown: false,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: 'absolute',
+          },
+          default: {},
+        }),
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Ionicons name="home" color={color} size={24} />,
+        }}
+      />
+    </Tabs>
+  );
+}
