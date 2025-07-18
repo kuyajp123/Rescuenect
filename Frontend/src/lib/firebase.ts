b2aa91f2557027af79c9754d03b0fe981d628fc9
John Paul Naag <kuyajp123@gmail.com>
2025-07-19 00:16:30 +0800

    generate new Firebase Admin SDK credentials

--- Frontend/src/lib/firebase.ts
+++ Frontend/src/lib/firebase.ts
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+const { env } = import.meta.env;
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyADtkf5UNXMgpxBfqyGQwVfiWePv8kdSEo",
-  authDomain: "rescuenect.firebaseapp.com",
-  projectId: "rescuenect",
-  storageBucket: "rescuenect.firebasestorage.app",
-  messagingSenderId: "193005721627",
-  appId: "1:193005721627:web:172cabd7ddfd2e399400be",
-  measurementId: "G-5G7JJRQ6QZ"
+  apiKey: env.VITE_FIREBASE_API_KEY,
+  authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.VITE_FIREBASE_APP_ID,
+  measurementId: env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
